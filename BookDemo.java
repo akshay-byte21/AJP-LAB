b9bf432b0751633a0c447ab4e1e448ae447a40e4
GANTAKURA AKSHAY GANESH <gmkakshay@gmail.com>
2026-08-04 15:34:17 +0530

    Add files via upload

---
+++ BookDemo.java
@@ -0,0 +1,37 @@
+class Author {
+    String name;
+    String email;
+    char gender;
+Author(String name,String email,char gender){
+        this.name=name;
+        this.email=email;
+        this.gender=gender;
+    }
+    
+}
+class Book {
+    String name;
+    Author author;
+     double price;
+     int stock;
+    Book(String name, Author author, double price, int stock) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.stock = stock;
+    }
+    
+}
+public class BookDemo {
+    public static void main(String args[]) {
+        Author a = new Author("Akshay", "akshay@gmail.com", 'm');
+        Book b = new Book("Advanced Java Programming", a, 780.0, 21);
+        System.out.println("Book Name : " + b.name);
+        System.out.println("Price : " + b.price);
+        System.out.println("Stock : " + b.stock);
+        System.out.println("Author Name : " + a.name);
+        System.out.println("Author Email : " + a.email);
+        System.out.println("Gender : " + a.gender);
+    }
+}
+
