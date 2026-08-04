3f66d403c86058e0b47422f023de8bc760ab024c
GANTAKURA AKSHAY GANESH <gmkakshay@gmail.com>
2026-08-04 15:19:10 +0530

    Add files via upload

---
+++ Animals.java
@@ -0,0 +1,38 @@
+class Animal{
+    void eat(){
+    System.out.println("Animal eats food");
+    }
+    
+    void sleep(){
+    System.out.println("Animal sleeps");
+    }
+    }
+    
+    class Bird extends Animal{
+    void eat(){
+    System.out.println("Bird eats seeds");
+    }
+    
+    void sleep(){
+    System.out.println("Bird sleeps in its nest");
+    }
+    
+    void fly(){
+    System.out.println("Bird flies in the sky");
+    }
+    }
+    
+    public class Animals{
+    public static void main(String[] args){
+    Animal animal = new Animal();
+    animal.eat();
+    animal.sleep();
+    
+    System.out.println();
+    
+    Bird bird = new Bird();
+    bird.eat();
+    bird.sleep();
+    bird.fly();
+    }
+    }
